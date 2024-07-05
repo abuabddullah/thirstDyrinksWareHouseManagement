040ccec58e0a8077ff79b86e0e43961e73d8beba
Asif A Owadud <asifaowadud@gmail.com>
2024-07-06 01:31:25 +0600

    live server link changed

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,13 +5,21 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey:process.env.REACT_APP_apiKey,
+//   authDomain:process.env.REACT_APP_authDomain,
+//   projectId:process.env.REACT_APP_projectId,
+//   storageBucket:process.env.REACT_APP_storageBucket,
+//   messagingSenderId:process.env.REACT_APP_messagingSenderId,
+//   appId:process.env.REACT_APP_appId,
+// };
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_apiKey,
-  authDomain:process.env.REACT_APP_authDomain,
-  projectId:process.env.REACT_APP_projectId,
-  storageBucket:process.env.REACT_APP_storageBucket,
-  messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  appId:process.env.REACT_APP_appId,
+  apiKey: "AIzaSyDQobKSHqUkd6eCxwr5HbboKcL9T6tuax4",
+  authDomain: "bookeventsmern.firebaseapp.com",
+  projectId: "bookeventsmern",
+  storageBucket: "bookeventsmern.appspot.com",
+  messagingSenderId: "215105021789",
+  appId: "1:215105021789:web:0a2378ce5073aa12b72ab4"
 };
 
 // Initialize Firebase
