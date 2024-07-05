4e8242c61402599838492d906960c30224eb757c
Asif A Owadud <asifaowadud@gmail.com>
2024-07-06 01:52:38 +0600

    live server link changed

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,21 +5,13 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey:process.env.REACT_APP_apiKey,
-//   authDomain:process.env.REACT_APP_authDomain,
-//   projectId:process.env.REACT_APP_projectId,
-//   storageBucket:process.env.REACT_APP_storageBucket,
-//   messagingSenderId:process.env.REACT_APP_messagingSenderId,
-//   appId:process.env.REACT_APP_appId,
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyDQobKSHqUkd6eCxwr5HbboKcL9T6tuax4",
-  authDomain: "bookeventsmern.firebaseapp.com",
-  projectId: "bookeventsmern",
-  storageBucket: "bookeventsmern.appspot.com",
-  messagingSenderId: "215105021789",
-  appId: "1:215105021789:web:0a2378ce5073aa12b72ab4"
+  apiKey: "AIzaSyAbld8fh9GG-JDCZu4juvcFevv-bvrdwtQ",
+  authDomain: "thirstydrinks-11.firebaseapp.com",
+  projectId: "thirstydrinks-11",
+  storageBucket: "thirstydrinks-11.appspot.com",
+  messagingSenderId: "202458021821",
+  appId: "1:202458021821:web:9f4e77fb1ad874effa5cd4"
 };
 
 // Initialize Firebase
